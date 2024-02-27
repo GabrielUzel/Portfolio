eb43b7d5455ba9dc2d793e09a010e2c06d5e9309
Gabriel <gabriel.uzel1@gmail.com>
2024-02-27 15:04:42 -0300

    Add image hover function on social media icons

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const imageLinkedin = document.getElementById("linkedin");
+const imageInstagram = document.getElementById("instagram");
+const imageGithub = document.getElementById("github");
+
+imageGithub.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    imageHoverIn(imageGithub)
+});
+imageLinkedin.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    imageHoverIn(imageLinkedin)
+});
+imageInstagram.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    imageHoverIn(imageInstagram)
+});
+
+imageGithub.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    imageHoverOut(imageGithub)
+});
+imageLinkedin.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    imageHoverOut(imageLinkedin)
+});
+imageInstagram.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    imageHoverOut(imageInstagram)
+});
+
+function imageHoverIn(img) {
+    if(img.id === "linkedin") { img.src = "./assets/img/linkedin-icon-activated.svg"; } 
+    else if(img.id === "instagram") { img.src = "./assets/img/instagram-icon-activated.svg"; } 
+    else { img.src = "./assets/img/github-icon-activated.svg"; }
+}
+
+function imageHoverOut(img) {
+    if(img.id === "linkedin") { img.src = "./assets/img/linkedin-icon-deactivated.svg"; } 
+    else if(img.id === "instagram") { img.src = "./assets/img/instagram-icon-deactivated.svg"; } 
+    else { img.src = "./assets/img/github-icon-deactivated.svg"; }
+} 
