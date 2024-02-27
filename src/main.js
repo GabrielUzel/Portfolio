2c973419f034cf2c73b6cdad064cafeb8e1dc792
Gabriel <gabriel.uzel1@gmail.com>
2024-02-27 17:32:26 -0300

    Change footer image hover function

--- src/main.js
+++ src/main.js
@@ -1,44 +1,11 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-
-const imageLinkedin = document.getElementById("linkedin");
-const imageInstagram = document.getElementById("instagram");
-const imageGithub = document.getElementById("github");
-
-imageGithub.addEventListener("mouseover", (event) => {
-    event.preventDefault();
-    imageHoverIn(imageGithub);
-});
-imageLinkedin.addEventListener("mouseover", (event) => {
-    event.preventDefault();
-    imageHoverIn(imageLinkedin);
-});
-imageInstagram.addEventListener("mouseover", (event) => {
-    event.preventDefault();
-    imageHoverIn(imageInstagram);
-});
-
-imageGithub.addEventListener("mouseout", (event) => {
-    event.preventDefault();
-    imageHoverOut(imageGithub);
-});
-imageLinkedin.addEventListener("mouseout", (event) => {
-    event.preventDefault();
-    imageHoverOut(imageLinkedin);
-});
-imageInstagram.addEventListener("mouseout", (event) => {
-    event.preventDefault();
-    imageHoverOut(imageInstagram);
-});
-
-function imageHoverIn(img) {
-    if(img.id === "linkedin") { img.src = "./assets/img/linkedin-icon-activated.svg"; } 
-    else if(img.id === "instagram") { img.src = "./assets/img/instagram-icon-activated.svg"; } 
-    else { img.src = "./assets/img/github-icon-activated.svg"; }
+function imageOnMouseover(img) {
+    if(img.id === "github") { img.src = "./public/assets/img/github-icon-activated.svg"; }
+    else if(img.id === "linkedin") { img.src = "./public/assets/img/linkedin-icon-activated.svg"; }
+    else { img.src = "./public/assets/img/instagram-icon-activated.svg"; }
 }
 
-function imageHoverOut(img) {
-    if(img.id === "linkedin") { img.src = "./assets/img/linkedin-icon-deactivated.svg"; } 
-    else if(img.id === "instagram") { img.src = "./assets/img/instagram-icon-deactivated.svg"; } 
-    else { img.src = "./assets/img/github-icon-deactivated.svg"; }
-} 
+function imageOnMouseOut(img) {
+    if(img.id === "github") { img.src = "./public/assets/img/github-icon-deactivated.svg"; }
+    else if(img.id === "linkedin") { img.src = "./public/assets/img/linkedin-icon-deactivated.svg"; }
+    else { img.src = "./public/assets/img/instagram-icon-deactivated.svg"; }
+}
