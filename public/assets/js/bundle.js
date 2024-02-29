d03bd2e9c46db29a65cc5ebc8cda074fe74991da
GabrielUzel <gabriel.uzel1@gmail.com>
2024-02-29 15:29:21 -0300

    Bug fixed: webpack not working

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+function imageOnMouseOver(img) {
+  if (img.id === "github") {
+    img.src = "./img/github-icon-activated.svg";
+  } else if (img.id === "linkedin") {
+    img.src = "./img/linkedin-icon-activated.svg";
+  } else {
+    img.src = "./img/instagram-icon-activated.svg";
+  }
+}
+function imageOnMouseOut(img) {
+  if (img.id === "github") {
+    img.src = "./img/github-icon-deactivated.svg";
+  } else if (img.id === "linkedin") {
+    img.src = "./img/linkedin-icon-deactivated.svg";
+  } else {
+    img.src = "./img/instagram-icon-deactivated.svg";
+  }
+}
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 function imageOnMouseOver(img) {
-    if(img.id === "github") { img.src = "./public/assets/img/github-icon-activated.svg"; }
-    else if(img.id === "linkedin") { img.src = "./public/assets/img/linkedin-icon-activated.svg"; }
-    else { img.src = "./public/assets/img/instagram-icon-activated.svg"; }
+    if(img.id === "github") { img.src = "./img/github-icon-activated.svg"; }
+    else if(img.id === "linkedin") { img.src = "./img/linkedin-icon-activated.svg"; }
+    else { img.src = "./img/instagram-icon-activated.svg"; }
 }
 
 function imageOnMouseOut(img) {
-    if(img.id === "github") { img.src = "./public/assets/img/github-icon-deactivated.svg"; }
-    else if(img.id === "linkedin") { img.src = "./public/assets/img/linkedin-icon-deactivated.svg"; }
-    else { img.src = "./public/assets/img/instagram-icon-deactivated.svg"; }
+    if(img.id === "github") { img.src = "./img/github-icon-deactivated.svg"; }
+    else if(img.id === "linkedin") { img.src = "./img/linkedin-icon-deactivated.svg"; }
+    else { img.src = "./img/instagram-icon-deactivated.svg"; }
 }
