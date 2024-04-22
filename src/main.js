3dd6258a56abe239811ca114400a8e4514e77625
Gabriel <gabriel.uzel1@gmail.com>
2024-04-22 16:22:57 -0300

    Update some styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ const htmlImage = document.getElementById("html");
 const cssImage = document.getElementById("css");
 const expressImage = document.getElementById("express");
 const mongoImage = document.getElementById("mongo");
+const cplusplusImage = document.getElementById("cplusplus");
+const javaImage = document.getElementById("java");
+const babelImage = document.getElementById("babel");
+const webpackImage = document.getElementById("webpack");
+const reactImage = document.getElementById("react");
 
 jsImage.addEventListener("mouseover", (event) => {
     event.preventDefault();
@@ -47,4 +52,49 @@ mongoImage.addEventListener("mouseover", (event) => {
 mongoImage.addEventListener("mouseout", (event) => {
     event.preventDefault();
     mongoImage.src = "./assets/img/skillsIcons/mongoIcon_noBackground.png";
+});
+
+cplusplusImage.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    cplusplusImage.src = "./assets/img/skillsIcons/cplusplusIcon.png";
+});
+cplusplusImage.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    cplusplusImage.src = "./assets/img/skillsIcons/cplusplusIcon_noBackground.png";
+});
+
+javaImage.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    javaImage.src = "./assets/img/skillsIcons/javaIcon.png";
+});
+javaImage.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    javaImage.src = "./assets/img/skillsIcons/javaIcon_noBackground.png";
+});
+
+babelImage.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    babelImage.src = "./assets/img/skillsIcons/babelIcon.png";
+});
+babelImage.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    babelImage.src = "./assets/img/skillsIcons/babelIcon_noBackground.png";
+});
+
+webpackImage.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    webpackImage.src = "./assets/img/skillsIcons/webpackIcon.png";
+});
+webpackImage.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    webpackImage.src = "./assets/img/skillsIcons/webpackIcon_noBackground.png";
+});
+
+reactImage.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    reactImage.src = "./assets/img/skillsIcons/reactIcon.png";
+});
+reactImage.addEventListener("mouseout", (event) => {
+    event.preventDefault();
+    reactImage.src = "./assets/img/skillsIcons/reactIcon_noBackground.png";
 });
